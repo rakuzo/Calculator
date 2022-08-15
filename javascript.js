dd78fedb40707fbd0d3ed42f016f6c4e2141168d
rakuzo <rakuzoxilion@gmail.com>
2022-08-15 12:36:28 +0700

    Add operate function

--- javascript.js
+++ javascript.js
@@ -13,3 +13,10 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, math) {
+    if (math === 'add') return add(a, b);
+    if (math === 'subtract') return subtract(a, b);
+    if (math === 'multiply') return multiply(a, b);
+    if (math === 'divide') return divide(a, b);
+}
