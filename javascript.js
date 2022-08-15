633391ab5ac37b85af3ec702dc1fbe3a1d70b90b
rakuzo <rakuzoxilion@gmail.com>
2022-08-15 16:22:05 +0700

    Add listener to number buttons

--- javascript.js
+++ javascript.js
@@ -20,3 +20,17 @@ function operate(a, b, math) {
     if (math === 'multiply') return multiply(a, b);
     if (math === 'divide') return divide(a, b);
 }
+
+const screen = document.querySelector('#screen');
+const numbs = document.querySelectorAll('#num');
+const equal = document.querySelector('#equal');
+
+numbs.forEach((numb) => {
+    numb.addEventListener('click', (e) => {
+        console.log(e.target.innerText);
+    });
+});
+
+equal.addEventListener('click', (e) => {
+    console.log(e.target.innerText);
+});
