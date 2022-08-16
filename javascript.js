81a7c8ee809206f6c9d1706e2fb2c0867ba34c68
Dornell <rakuzoxilion@gmail.com>
2022-08-16 13:27:32 +0700

    Implement "+" and "=" in the screen

--- javascript.js
+++ javascript.js
@@ -14,7 +14,7 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(a, b, math) {
+function operate(a, math, b) {
     if (math === 'add') return add(a, b);
     if (math === 'subtract') return subtract(a, b);
     if (math === 'multiply') return multiply(a, b);
@@ -23,24 +23,32 @@ function operate(a, b, math) {
 
 const screen = document.querySelector('#screen');
 const numbs = document.querySelectorAll('#num');
+const plus = document.querySelector('#add');
 const equal = document.querySelector('#equal');
 let displayValue = '';
+const tempMemory = {
+    firstNum: 0,
+    operator: '',
+    secondNum: 0
+};
 
 numbs.forEach((numb) => {
     numb.addEventListener('click', (e) => {
         screen.innerText += e.target.innerText;
-        return tempNumber();
+        return getNumScreen();
     });
 });
 
 equal.addEventListener('click', (e) => {
-    console.log(e.target.innerText);
+    // console.log(e.target.innerText);
+    screen.innerText += e.target.innerText;
 });
 
-// let tempNumber = function() {
-//     return screen.innerText;
-// };
+plus.addEventListener('click', (e) => {
+    // console.log(e.target.id);
+    screen.innerText += e.target.innerText;
+});
 
-function tempNumber() {
+function getNumScreen() {
     return displayValue = screen.innerText;
 };
